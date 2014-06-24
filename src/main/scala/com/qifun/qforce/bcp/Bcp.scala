5e4ee8ccbf19d8138361fd601375c9e6466a3c09
杨博 <pop.atry@gmail.com>
2014-06-24 15:46:35 +0800

    增加注释

--- src/main/scala/com/qifun/qforce/bcp/Bcp.scala
+++ src/main/scala/com/qifun/qforce/bcp/Bcp.scala
@@ -4,8 +4,61 @@ import java.nio.ByteBuffer
 
 /**
  * BCP协议相关的数据结构和常量
+ * 
+ * BCP（Brutal Control Protocol，残暴控制协议）是基于TCP实现的用户层传输协议。
+ * 
+ * BCP和普通TCP功能相似，重新实现了TCP的包确认重发机制。
+ * 
+ * 当网络条件不好，丢包频繁，TCP重传间隔时间会变得很长，从而基本不可用。
+ * 这种情况下，BCP会暴力杀死底层TCP连接，强制重建TCP连接，暴力重发数据，避免丢失数据并减少延时。
+ * 
+ * BCP和TCP都属于面向连接会话的协议。
+ * 但另一方面，但BCP数据不是TCP那样的流，而以[[Packet]]为单位。
+ * BCP协议不保证接收方收取[[Packet]]的顺序和发送方发出[[Packet]]的顺序一致。
+ * 这是因为一条BCP会话，可能会对应多达[[MaxConnectionsPerSession]]个底层TCP连接，
+ * 而这几条底层TCP连接的延时可能并不相同。
+ * 
+ * 一个BCP会话的过程如下：
+ *
+ *  1. 客户端随机生成[[NumBytesSessionId]]字节的会话ID。
+ *  1. 客户端发起TCP连接。
+ *  1. TCP连接成功建立后，客户端发送[[NumBytesSessionId]]字节的会话ID。
+ *  1. 客户端再发送一个varint格式的底层连接ID。
+ *     每条会话的底层连接ID都从0开始，
+ *     每当客户端发起新TCP连接时，底层连接ID递增1。
+ *  1. 客户端或服务端发送[[Data]]包，向对端发送用户数据。
+ *  1. 每发一个[[Data]]，对端都回应[[Acknowledge]]。
+ *  1. 如果客户端或服务端连续[[ServerHeartBeatDelay]]时间，
+ *     都没有[[Packet]]需要发送，那么发送一个[[HeartBeat]]到对端。
+ *  1. 如果客户端或服务端连续[[ServerReadingTimeout]]都收不到任何数据，
+ *     就认为这个TCP连接已经没救了，关闭这个TCP连接。
+ *  1. 当客户端认为已有底层TCP连接速度太慢时，客户端可以发起新的TCP连接。
+ *  1. 新增TCP连接与首个TCP一样，仍然属于同一会话，
+ *  1. 客户端也需要在连接建立后发送[[NumBytesSessionId]]字节的会话ID，
+ *     和varint格式的底层连接ID。
+ *     但此处的会话ID应当重用原有的会话ID，不要生成新ID。
+ *  1. 如果客户端发现某一会话中的TCP连接长期空闲，
+ *     那么客户端可以向服务端发送[[Finish]]，关掉一部分TCP连接。
+ *  1. 服务器收到[[Finish]]时，应当回复[[Acknowledge]]，然后关掉底层连接。
+ *  1. 如果一个底层TCP连接因为超时或者别的原因异常中断，
+ *     那么每一端都必须在同一会话的其他TCP连接上，
+ *     重发那些尚未收到对应[[Acknowledge]]的[[Data]]和[[Finish]]
+ *     重发时，应当发送[[RetransmissionData]]和[[RetransmissionFinish]]包。
+ *     重发的包中，包含原先连接ID和原先包ID。
+ *     包ID是指原包的序号，从零开始计算，
+ *     原连接每发送一个[[Data]]或[[Finish]]，序号就递增1。
+ *  1. 当客户端和服务端希望结束BCP会话时，向对端发送[[ShutDown]]，
+ *     然后清除该会话相关的所有数据。
+ *  1. 对端收到[[ShutDown]]后，无须回复，直接清除会话相关的所有数据。
+ *
+ * 此外，客户端可以把会话ID记录在文件中。
+ * 如果应用崩溃，用户在短时间内重连，
+ * 客户端可以使用先前保存的会话ID，而不需要重新生成会话ID。
+ * 这种情况下，客户端应当在建立连接后立即发送[[Renew]]给服务端，
+ * 服务端会放弃重发原会话尚未成功发送的数据，
+ * 但同时会视为会话从未断开，不会清除原会话的服务端数据。
  */
-private[bcp] object Bcp {
+object Bcp {
 
   /**
    * 最多缓存多少个离线包
@@ -29,7 +82,7 @@ private[bcp] object Bcp {
   final val ServerHeartBeatDelay: FiniteDuration = 3.seconds
 
   /**
-   * 服务器多长时间收不到心跳包就杀掉TCP连接。
+   * 多长时间收不到任何包，就杀掉TCP连接。
    *
    * @group Constants
    */
